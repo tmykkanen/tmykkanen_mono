b64dc781d01fa07a878d97c97a2fb3828d07f208
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-03-19 08:20:06 -0500

    feat: install astro in astro_sojourners

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,13 @@
 import js from '@eslint/js'
 import svelte from 'eslint-plugin-svelte'
+import astro from 'eslint-plugin-astro'
 import globals from 'globals'
 import ts from 'typescript-eslint'
 
 export const config = ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
+  ...astro.configs.recommended,
   ...svelte.configs['flat/recommended'],
   {
     languageOptions: {
