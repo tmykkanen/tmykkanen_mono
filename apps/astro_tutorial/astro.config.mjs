e662fcba26311c3af1f9d6c3cde0b007e731681e
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-03-19 16:40:11 -0500

    chore: cleanup ts warnings and set up projects

--- apps/astro_tutorial/astro.config.mjs
+++ apps/astro_tutorial/astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
-import tailwindcss from '@tailwindcss/vite'
-
 import svelte from '@astrojs/svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [svelte()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
-
-	integrations: [svelte()],
 })
