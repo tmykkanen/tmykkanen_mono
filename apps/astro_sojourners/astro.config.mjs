b95c99f4fecd3db6a4661a2ee58d9c94c17db8a8
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-03-19 18:42:55 -0500

    feat: added astro-icon

--- apps/astro_sojourners/astro.config.mjs
+++ apps/astro_sojourners/astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwindcss from '@tailwindcss/vite'
 
+import icon from 'astro-icon'
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte()],
+	integrations: [svelte(), icon()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
